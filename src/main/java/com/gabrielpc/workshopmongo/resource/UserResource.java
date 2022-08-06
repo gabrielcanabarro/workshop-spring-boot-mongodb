b5b316399e7b56d59e5b649a0f7f099f8a1ecb13
Gabriel <gcanabarro7@hotmail.com>
2022-08-06 17:56:16 -0300

    Insercao do modo DELETE

--- src/main/java/com/gabrielpc/workshopmongo/resource/UserResource.java
+++ src/main/java/com/gabrielpc/workshopmongo/resource/UserResource.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,24 +27,33 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
 	
-	@RequestMapping(value = "/{id}" ,method = RequestMethod.GET)
+	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	@RequestMapping(method = RequestMethod.POST)
+
+
+	@PostMapping
 	public ResponseEntity<Void> findById(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	
+	
 	
 }
 }
--- src/main/java/com/gabrielpc/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielpc/workshopmongo/services/UserService.java
@@ -32,6 +32,14 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete (String id) {
+		findById(id);
+		if (id == null) {
+			throw new ObjectNotFoundExcepetion("Objeto não encontrado");
+		}
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 }
